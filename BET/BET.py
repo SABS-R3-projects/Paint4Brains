ea418ec5dd8e5bbe82147f466c992ee1692840e4
sabs-r3 <itaimuzh@gmail.com>
2019-12-08 14:18:37 +0000

    Changed the paths to image files

--- BET/BET.py
+++ BET/BET.py
@@ -4,12 +4,12 @@ import nibabel as nb
 from deepbrain import Extractor
 import numpy as np
 
-skullstrip = BET(in_file="/home/sabs-r3/Desktop/BET/80yearold.nii",
-                 out_file="/home/sabs-r3/Desktop/BET/80yearold_bet.nii.gz",
+skullstrip = BET(in_file="80yearold.nii",
+                 out_file="80yearold_bet.nii.gz",
                  mask=False, robust = True)
 res = skullstrip.run()
 
-img = nb.load("/home/sabs-r3/Desktop/BET/80yearold.nii").get_fdata()
+img = nb.load("80yearold.nii").get_fdata()
 ext = Extractor()#I made changes to extractor to beacause tensorflow has been updated
 prob = ext.run(img) #Probabilities of brain tissue
 mask2 = np.where(prob>0.5, 1, prob*0)#An outline of the extracted brain
