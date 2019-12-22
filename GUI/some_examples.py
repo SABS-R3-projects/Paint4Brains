d4d36f12b484b2a97f493e2bb521980a127898f0
sabs-r3 <spamspambrennan@gmail.com>
2019-12-22 10:02:23 +0000

    Added a jupyter notebook with stuff and merged all changes from master

--- GUI/some_examples.py
+++ GUI/some_examples.py
@@ -1,4 +1,4 @@
 import pyqtgraph.examples
-# This is great. Provides functioning examples with working code
+# This is great. Provides functioning examples with working code.
 pyqtgraph.examples.run()
 
