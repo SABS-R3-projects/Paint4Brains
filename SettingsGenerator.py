8f8a58711273372b51dc78fcd4ee930b48d4abbb
AndreiRoibu <aroibu1@gmail.com>
2020-01-22 16:02:16 +0000

    created settings generator

--- SettingsGenerator.py
+++ SettingsGenerator.py
@@ -25,7 +25,7 @@ def get_current_directory():
 def read_original_configurator():
 
     """
-    This function reads the original configuration file anre returns all the values contained within in.
+    This function reads the original configuration file and returns all the values contained within in.
 
     Args:
         None
@@ -34,7 +34,7 @@ def read_original_configurator():
         None
     
     Returns:
-        dir_path (str): string containing the full path where the current script is contained in.
+        setting_dictionary (dict): dictionary containing the original configuration file information
 
     """
 
@@ -52,31 +52,20 @@ def read_original_configurator():
 
     options = config.options(sections[0])
 
-    print("Sections:", sections)
-    print("Options:", options)
-
     for option in options:
-        print(option)
-        print(config[sections][option])
+        try:
+            settings_dictionary[option] = config[sections[0]][option]
+            if settings_dictionary[option] == -1:
+                DebugPring("skip: %s" % option)
+        except:
+            print("Exception on %s!" % option)
+            settings_dictionary[option] = None
 
-    # for option in options:
-    #     settings_dictionary[option] = config.get(sections,option)
-        # try:
-        #     settings_dictionary[option] = config.get(section,option)
-        #     if settings_dictionary[option] == -1:
-        #         DebugPring("skip: %s" % option)
-        # except:
-        #     print("Exception on %s!" % option)
-        #     settings_dictionary[option] = None
-
-    # print(settings_dictionary)
-
-    print(options)
-    print(config.sections())
+    return sections[0], settings_dictionary
 
 def parser_configurator():
     """
-    This function returns the full path of the directory where the current script is contained in. This will be later relevant, for generating the settings_eval.ini function.
+    This function prints out the current settings, asks for a user input to update the settings and creates a new settings file.
 
     Args:
         None
@@ -87,30 +76,64 @@ def parser_configurator():
     Returns:
         dir_path (str): string containing the full path where the current script is contained in.
 
+    This function will need to be changed once the GUI settings window is created! 
+
     """
 
     directory_path = get_current_directory()
 
+    section_name, settings_dictionary = read_original_configurator()
+
     config = configparser.ConfigParser()
 
-    config['DEFAULT'] = {
-        'ServerAliveInterval': '45',
-        'Compression': 'yes',
-        'CompressionLevel': '9'
-        }
-    config['bitbucket.org'] = {}
-    config['bitbucket.org']['User'] = 'hg'
-    config['topsecret.server.com'] = {}
-    topsecret = config['topsecret.server.com']
-    topsecret['Port'] = '50022'     # mutates the parser
-    topsecret['ForwardX11'] = 'no'  # same here
-    config['DEFAULT']['ForwardX11'] = 'yes'
+    inputs = []
 
-    with open('example.ini', 'w') as configfile:
-        config.write(configfile)
+    for key in settings_dictionary:
+        if key == 'device':
+            print("Provide CPU or ID of GPU (0 or 1) you want to excecute your code, or press Enter to leave default")
+            data_input = input("Your input:")
+            if data_input == "":
+                single_input = settings_dictionary[key]
+            else:
+                single_input = data_input
+        elif key == 'coronal_model_path':
+            single_input = settings_dictionary[key]
+        elif key == 'axial_model_path':
+            single_input = settings_dictionary[key]
+        elif key == 'data_dir':
+            single_input = '"'+directory_path+'/quickNAT_pythorch/data_input'+'"'
+        elif key == 'directory_struct':
+            print("Valid options for data directory structure are >> FS <<  or >> Linear <<. If you input data directory is similar to FreeSurfer, i.e. data_dir/<Data_id>/mri/orig.mgz then use >> FS <<. If the entries are data_dir/<Data_id> use >> Linear <<. To leave at default, press Enter")
+            data_input = input("Provide information about input directory structure: ")
+            if data_input == "":
+                single_input = settings_dictionary[key]
+            else:
+                single_input = data_input
+        elif key == 'volumes_txt_file':
+            single_input = settings_dictionary[key]
+        elif key == 'batch_size':
+            single_input = settings_dictionary[key]
+        elif key == 'save_predictions_dir':
+            single_input = '"'+directory_path+'/quickNAT_pythorch/output_file'+'"'
+        elif key == 'view_agg':
+            single_input = settings_dictionary[key]
+        elif key == 'estimate_uncertainty':
+            single_input = settings_dictionary[key]
+        elif key == 'mc_samples':
+            single_input = settings_dictionary[key]
 
+        inputs.append(str(single_input))
+    
+    
+    cfgfile = open("settings_eval.ini",'w')
+    config.add_section(section_name)
 
+    for idx, key in enumerate(settings_dictionary):
+        config.set(section_name, key ,inputs[idx])
+
+    config.write(cfgfile)
+    cfgfile.close()
 
 if __name__=='__main__':
 
-    read_original_configurator()
+    parser_configurator()
