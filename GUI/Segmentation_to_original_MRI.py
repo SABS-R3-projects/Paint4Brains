8c452c9bb05c8a6de1c074fed59623db311daf7f
sabs-r3 <itaimuzh@gmail.com>
2020-02-05 15:00:49 +0000

    Can now superimpose segmentation mask to original unprocessed mri image

--- GUI/Segmentation_to_original_MRI.py
+++ GUI/Segmentation_to_original_MRI.py
@@ -1,25 +1,14 @@
 import nibabel as nib
 import os
-import numpy as np
 import nilearn as nl
 
-#Trying to resample the segmentation to the original image that has not been preprocessed
-os.chdir('/home/sabs-r3/Desktop')
-mask = nib.load('segmented.nii.nii.gz')
+#Resamples the segmented mask so that it can be overlaid to the original  un-processed image
+os.chdir('/home/sabs-r3/Desktop/')
+mask = nib.load('segmented.nii.gz')
 original = nib.load('MCI_M_77.nii')
 dimensions = original.header['dim']
-shape = [dimensions[1], dimensions[2], dimensions[3]]
+shape = original.get_data().shape
 pixel_size = original.header['pixdim']
 new_zooms = (pixel_size[1], pixel_size[2], pixel_size[3])
-target_affine = nib.volumeutils.shape_zoom_affine(shape, new_zooms)
-new_mask = nl.image.resample_img(mask, target_affine, target_shape=shape, interpolation='nearest')
-
-
-orientation = nib.orientations.axcodes2ornt(nib.aff2axcodes(new_mask.affine))
-target_orientation = nib.orientations.axcodes2ornt(nib.aff2axcodes(target_affine))
-transformation = nib.orientations.ornt_transform(orientation, target_orientation)
-data = new_mask.get_fdata()
-new_affine = target_affine
-new_tran = nib.orientations.apply_orientation(data, transformation)
-transformed_image = nib.Nifti1Image(new_tran, new_affine)
-nib.save(transformed_image, 'Segmentated_to_Original_MRI.nii')
+new_mask = nl.image.resample_img(mask, original.affine, target_shape=shape, interpolation='nearest')
+nib.save(new_mask, 'Segmentated_to_Original_MRI.nii')
