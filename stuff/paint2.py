7d566d1d80ecbd126d284a936e998ddc01ecfcda
sabs-r3 <spamspambrennan@gmail.com>
2020-04-29 20:43:47 +0100

    :moyai: Looking at painting options

--- stuff/paint2.py
+++ stuff/paint2.py
@@ -1,6 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QSlider, QHBoxLayout, QLabel
-from PyQt5.QtGui import QIcon, QImage, QPainter, QPen, QBrush, QPixmap
-from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
 import numpy as np
 import pyqtgraph as pg
 import sys
@@ -12,7 +11,8 @@ rubber = np.array([[-1]]).astype(np.int8)
 class PickBrush(QWidget):
     def __init__(self):
         super(PickBrush, self).__init__()
-        self.layout = QHBoxLayout(self)
+        self.setSizePolicy(QSizePolicy.Minimum,QSizePolicy.Minimum)
+        self.layout = QVBoxLayout(self)
 
         self.g_item = pg.GraphicsView()
         self.vbox = pg.ViewBox()
@@ -26,8 +26,30 @@ class PickBrush(QWidget):
         self.g_item.setCentralItem(self.vbox)
         self.layout.addWidget(self.g_item)
 
-    def mouseDoubleClickEvent(self, a0):
-        self.img.setDrawKernel(dot, mask=rubber, center=(0, 0), mode='add')
+        self.hlayout = QHBoxLayout()
+        self.label = QLabel("Size")
+        self.label.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.hlayout.addWidget(self.label)
+        self.pen_size = QLineEdit("20")
+        self.pen_size.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.pen_size.textChanged.connect(self.new_matrix_size)
+        self.hlayout.addWidget(self.pen_size)
+
+        self.buttn = QPushButton("DONE")
+        self.buttn.clicked.connect(self.close)
+        self.buttn.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Minimum)
+        self.hlayout.addWidget(self.buttn)
+
+        self.layout.addLayout(self.hlayout)
+
+    def new_matrix_size(self):
+        val = int(self.pen_size.text())
+        self.pen = np.zeros((val, val))
+        self.img.setImage(self.pen)
+        self.img.setLevels([0, 2])
+
+
+
 
 
 
