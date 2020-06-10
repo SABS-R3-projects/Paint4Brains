261376a43dfb0cc764640da5c4184bfbf9d28e99
sabs-r3 <spamspambrennan@gmail.com>
2020-06-10 15:58:27 +0100

    :moyai: Incorporating the Intensity Histogram. It now pops up as a new window.

--- Paint4Brains/GUI/HistogramWidget.py
+++ Paint4Brains/GUI/HistogramWidget.py
@@ -5,6 +5,7 @@ import numpy as np
 import nibabel as nib
 import os
 
+
 class HistogramWidget(QWidget):
 
     def __init__(self, viewer):
@@ -14,14 +15,15 @@ class HistogramWidget(QWidget):
         self.layout = QtWidgets.QVBoxLayout(self)
         self.setWindowTitle("Intensity Correction Histogram")
         self.graphWidget = pg.PlotWidget()
-        y, x = np.histogram(self.brain.data, bins=255, range=(0., 1.),density= True)
-        self.graphWidget.plot(x[:-1], y, range=(0., 1.), density = True)
+        y, x = np.histogram(self.brain.data, bins=255, range=(0., 1.), density=True)
+        self.graphWidget.plot(x, y, range=(0., 1.), density=True, stepMode=True)
         self.graphWidget.setLabel('left', 'Voxel Density', size=30)
         self.graphWidget.setLabel('bottom', 'Voxel Intensity', size=30)
         self.layout.addWidget(self.graphWidget)
 
         self.minimum = 0.5
         self.maximum = 1.6
+        self.step_size = 0.1
 
         self.hlayout = QtWidgets.QHBoxLayout()
         self.label = QtWidgets.QLabel()
@@ -33,14 +35,13 @@ class HistogramWidget(QWidget):
         self.log_intensity_slider = QtWidgets.QSlider()
         self.log_intensity_slider.setOrientation(QtCore.Qt.Horizontal)
         self.log_intensity_slider.setObjectName("log_intensity_slider")
-        self.log_intensity_slider.setMinimum(int(self.minimum*10))
-        self.log_intensity_slider.setMaximum(int(self.maximum*10))
-        self.log_intensity_slider.setValue(10)
+        self.log_intensity_slider.setMinimum(int(self.minimum/self.step_size))
+        self.log_intensity_slider.setMaximum(int(self.maximum/self.step_size))
+        self.log_intensity_slider.setValue(int(1.0/self.step_size))
         self.log_intensity_slider.setSingleStep(1)
         self.hlayout.addWidget(self.log_intensity_slider)
         self.layout.addLayout(self.hlayout)
         self.log_intensity_slider.valueChanged.connect(self.update_intensity)
-
 
     def update_intensity(self):
         """Intensity Update
@@ -50,15 +51,14 @@ class HistogramWidget(QWidget):
         """
         # Take input from controller
         value = self.log_intensity_slider.value()
-        self.brain.intensity = float(value)/10
+        self.brain.intensity = float(value) * self.step_size
         # Edit the Brain Data Using function defined in BrainData class
         self.brain.log_normalization()
-        y, x = np.histogram(self.brain.data, bins=255, range=(0., 1.), density= True)
+        y, x = np.histogram(self.brain.data, bins=255, range=(0., 1.), density=True)
         # Update what you are displaying on histogram window
         self.graphWidget.clear()
-        self.graphWidget.plot(x[:-1], y, range=(0., 1.), density = True)
+        self.graphWidget.plot(x, y, range=(0., 1.), density=True, stepMode=True)
         self.label.setText(
             "Intensity Level: {0:.1f}".format(self.brain.intensity))
         # Update what you are displaying on brain image
         self.win.refresh_image()
-
