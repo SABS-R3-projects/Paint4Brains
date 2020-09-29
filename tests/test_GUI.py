a416e8cea9cfe50355cddfa2d6dabc8e8fc724b9
sabs-r3 <spamspambrennan@gmail.com>
2020-09-29 12:06:47 +0100

    :moyai: Tested a few basic GUI functions. This testing is very limited as you can only click on buttons.

--- tests/test_GUI.py
+++ tests/test_GUI.py
@@ -10,25 +10,27 @@ from Paint4Brains.GUI.MainWidget import MainWidget
 
 
 class TestMainWindow(unittest.TestCase):
-    """Test class methods in GUI
+    """Test a few GUI methods.
+
+    Due to the limitations of testing PyQt5 applications with QTest the tests here a quite limited.
     """
     root_dir = os.path.dirname(os.path.dirname(__file__))
     filename = os.path.join(root_dir, '../Paint4Brains/opensource_brains/H_F_22.nii')
     app = QApplication(['-platform', 'minimal'])
     main = MainWindow(filename)
 
-    def test_MainWindow(self):
-        """Testing the main window opens
-        """
-        # First open MainWindow:
-        self.main.show()
-        QTest.qWaitForWindowExposed(self.main)
-
     def test_MainWindow_title(self):
         """Simple test to test that testing is feasible.
         """
         # For MainWindow check the title:
         self.assertEqual(self.main.windowTitle(), "Paint4Brains")
+
+    def test_MainWindow_starts(self):
+        """Testing that the GUI starts as expected.
+        """
+        # Try and open MainWindow:
+        self.main.show()
+        QTest.qWaitForWindowExposed(self.main)
 
     def test_MainWindow_shortcuts(self):
         """Testing some of the simpler shortcuts do what you would expect them to
@@ -57,9 +59,9 @@ class TestMainWindow(unittest.TestCase):
         QTest.keyClick(self.main, "h", Qt.ControlModifier)
         assert self.main.hist_widget.isVisible() != old
 
-
     def test_view_recentering(self):
-
+        """Testing the image recenter function
+        """
         main_widget = MainWidget(self.main.brain)
 
         # Where is the brain usually?
@@ -69,10 +71,10 @@ class TestMainWindow(unittest.TestCase):
         main_widget.win.view.setRange(QRect(0, 0, 2 * usual.height(), 2 * usual.width()))
 
         # Size should be different
-        self.assertNotEqual(usual, main_widget.win.view.viewRect())
+        assert usual != main_widget.win.view.viewRect()
 
         # Recenter
         main_widget.win.recenter()
 
         # Size should be the same now
-        self.assertEqual(usual, main_widget.win.view.viewRect())
+        assert usual == main_widget.win.view.viewRect()
