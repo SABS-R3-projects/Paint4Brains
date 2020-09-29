c68910f2ebfe947493a3afc9e0003a0ee9767710
Pavan Chaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2020-09-29 12:01:15 +0100

    changing master doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,3 +60,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+#setting master doc to index
+master_doc = 'index'
